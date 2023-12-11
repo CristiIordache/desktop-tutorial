f9777297ec0f209f21199ac72062f8603a1bdf75
CristiIordache <145381618+CristiIordache@users.noreply.github.com>
2023-12-11 19:28:11 +0000

    js

---
+++ javascript/11.12.23.js
@@ -0,0 +1,38 @@
+// console.log("hello!");
+
+
+//  console.log("hai text ");
+// console.log("Cristi");
+// console.error("avem o eroare ");
+// console.warn(" mare eroare ");
+
+//  alert("alerta!");
+
+
+// document.getElementById("numaru1").innerText = "text setat culoare "
+// let output = document.getElementById("numaru1");
+// output.innerText = "text setat culoare "
+// output.style.color = red;
+
+// let este sa folososesti o derivata
+// constnt este sa sa stea lucru ne schimbat
+
+
+// string este text
+// umber este numar
+
+// boolean = true/false
+
+let age = 11
+age++
+console.log(age)
+
+let name= "cristi"
+// console.log(cristi)
+
+let namee=typeof(nameee)
+
+age*=2
+age/=3
+console.log(age * 2)
+console.log(age/3)
