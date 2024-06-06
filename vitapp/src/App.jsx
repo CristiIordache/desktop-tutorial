7651bcdfd60186cc24170202631dc4843d1c4e00
CristiIordache <145381618+CristiIordache@users.noreply.github.com>
2024-06-06 18:27:39 +0100

    a

---
+++ vitapp/src/App.jsx
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import './App.css'
+import CounterButton from './componente/indentificare/regist/register';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to my app</h1>
+        <CounterButton />
+      </header>
+    </div>
+  );
+}
+
+export default App;
