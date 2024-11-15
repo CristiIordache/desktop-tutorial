4f3cea81ae57eb657afd5efa48a2a8ae99391a03
CristiIordache <145381618+CristiIordache@users.noreply.github.com>
2024-11-15 13:17:45 +0000

    s

--- parking/utils/paymentUtils.js
+++ parking/utils/paymentUtils.js
@@ -1,7 +1,7 @@
 module.exports = {
     calculateFee: function(entryTime, currentTime) {
       const freeHours = 2;
-      const hourlyRate = 10; // Stabilim un tarif de 10 unități pe oră
+      const hourlyRate = 10; // Stabilim un tarif de 10 euro pe oră
   
       const diffInMilliseconds = currentTime - entryTime;
       const diffInHours = Math.ceil(diffInMilliseconds / (1000 * 60 * 60));
